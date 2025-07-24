fb2b13b3aff5fd9aa6e480e59ede28d698be10e7
Kate <kmc.macguire@gmail.com>
2025-07-24 11:25:36 -0600

    Linked a second view!

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,12 +12,15 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Text("This is root view")
-                NavigationLink(destination: Text("You've arrived to the Second View")) {
+                NavigationLink(destination: SecondView()) {
                     Text("Click me!")
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
                 }
-             }
-           }
+              }
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
         }
     }
 
